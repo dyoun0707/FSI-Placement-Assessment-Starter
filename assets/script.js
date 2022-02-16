7ea1c7a23278a7fd0ad174f7ca739be3f7866e31
dyoun0707 <daryoun07@gmail.com>
2022-02-15 21:33:00 -0500

    assessment 1

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,30 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Darryl Young" // HINT: Replace this with your own name!
 
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
-
-// selecting the element with an id of credit
-const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
-
-// Code to update name display
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+function updateQuantity(displayQuantity) {
+    let quantity = document.querySelector('.total-quantity')
+    quantity.innerHTML = displayQuantity 
+}
+let quantity = 1
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+// Buttons
+const quantDown = document.querySelector('#quantity-down')
+const quantUp = document.querySelector('#quantity-up')
+
+// add event listener to decrease quantity
+quantDown.addEventListener('click', function(e){
+    if (quantity > 0) {
+        quantity--
+    }
+    updateQuantity(`Quantity: ${quantity}`)
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+// add event listener to increase quantity
+quantUp.addEventListener('click', function(){
+    quantity++
+    updateQuantity(`Quantity: ${quantity}`)
+})
