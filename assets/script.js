1b336d703c0ee4ff27b4bba0e312cd14b7a83fea
dyoun0707 <daryoun07@gmail.com>
2022-02-17 13:28:02 -0500

    gcs

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,56 @@ let yourName = "Darryl Young" // HINT: Replace this with your own name!
 
 credit.textContent = `Created by ${yourName}`
 
-const gingerbread = document.querySelector(".gingerbread")
-const chocolatechip = document.querySelector(".chocolatechip")
-const sugarsprinnkle = document.querySelector(".sugarsprinkle")
+// We'll use these variables to track the counts of each cookie type
+let gb = 0 // Gingerbread
+let cc = 0 // Chocolate Chip
+let sugar = 0 // Sugar Sprinkle
 
-gingerbread.addEventListener("click")
+const gbPlusBtn = document.querySelector('#gingerbread')
+const ccPlusBtn = document.querySelector('#chocolatechip')
+const sugarPlusBtn = document.querySelector('#sugarsprinkle')
+console.log('Btn')
+
+const gbNum = document.querySelector('#gbNum')
+const ccNum = document.querySelector('#ccNum')
+const sugarNum = document.querySelector('#sugarNum')
+console.log('Num')
+
+gbPlusBtn.addEventListener('click', function(){
+    gb = gb +1
+    console.log('gbcount')
+    gbNum.textContent = gb
+})
+
+ccPlusBtn.addEventListener('click', function(){
+    cc = cc +1
+    console.log('cccount')
+    ccNum.textContent = cc
+})
+
+
+sugarPlusBtn.addEventListener('click', function(){
+    sugar = sugar +1
+    console.log('sugarcount')
+    sugarNum.textContent = sugar
+})
+
+gbPlusBtn.addEventListener('click', function(){
+    gb = gb -1
+    console.log('gbcount')
+    gbNum.textContent = gb
+})
+
+ccPlusBtn.addEventListener('click', function(){
+    cc = cc -1
+    console.log('cccount')
+    ccNum.textContent = cc
+})
+
+
+sugarPlusBtn.addEventListener('click', function(){
+    sugar = sugar -1
+    console.log('sugarcount')
+    sugarNum.textContent = sugar
+})
+
