70d7a9e63ba39e23afe948636257324fd3ad107a
dyoun0707 <daryoun07@gmail.com>
2022-02-17 10:43:01 -0500

    lister1

--- assets/script.js
+++ assets/script.js
@@ -5,26 +5,8 @@ let yourName = "Darryl Young" // HINT: Replace this with your own name!
 
 credit.textContent = `Created by ${yourName}`
 
-function updateQuantity(displayQuantity) {
-    let quantity = document.querySelector('.total-quantity')
-    quantity.innerHTML = displayQuantity 
-}
-let quantity = 1
+const gingerbread = document.querySelector(".gingerbread")
+const chocolatechip = document.querySelector(".chocolatechip")
+const sugarsprinnkle = document.querySelector(".sugarsprinkle")
 
-// Buttons
-const quantDown = document.querySelector('#quantity-down')
-const quantUp = document.querySelector('#quantity-up')
-
-// add event listener to decrease quantity
-quantDown.addEventListener('click', function(e){
-    if (quantity > 0) {
-        quantity--
-    }
-    updateQuantity(`Quantity: ${quantity}`)
-})
-
-// add event listener to increase quantity
-quantUp.addEventListener('click', function(){
-    quantity++
-    updateQuantity(`Quantity: ${quantity}`)
-})
+gingerbread.addEventListener("click")
